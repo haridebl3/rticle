f80a5353ad198348af01e444bc8a351d21db1679
haridebl3 <hariharan261199@gmail.com>
2021-10-19 20:51:42 +0530

    Hariharan | Allow cors

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const userRoute = require("./src/routes/user");
 const bufferRoute = require("./src/routes/buffer");
 const articles = require("./src/models/ArticleModel");
 const articleRoute = require("./src/routes/article");
-const port = process.env.PORT || 4000;
 const app = express();
-
-app.set("port", port || 4000);
-app.set("host", "localhost");
 
 app.use(bodyParser.json());
 var mongoose = require("mongoose");
@@ -30,6 +26,7 @@ mongoose
     process.exit(1);
   });
 app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "x-access-token, Origin, Content-Type, Accept"
@@ -41,6 +38,6 @@ app.use("/buffer", bufferRoute);
 app.use("/article", articleRoute);
 app.use("/images", express.static(path.join(__dirname, "src/images")));
 
-app.listen(port, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log(`Node server is listening on port ${port}`);
 });
